96be4b4f63a173196d06d56c7551fb919099a82d
Parekh_Harsh <92668837+Harsh8503@users.noreply.github.com>
2025-11-08 16:25:36 +0530

    -add fields

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -10,5 +10,7 @@ class Book extends Model
 
     protected $fillable = [
         'name',
+        'author',
+        'publisher',
     ];
 }
--- database/migrations/2025_11_08_102658_create_books_table.php
+++ database/migrations/2025_11_08_102658_create_books_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('author');
+            $table->string('publisher');
             $table->timestamps();
         });
     }
